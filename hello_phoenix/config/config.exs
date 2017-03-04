f1951f7f5757ed1a1c6785b62b6a376a367e1cbb
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-03-04 22:58:06 +0600

    Initial commit
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

---
+++ hello_phoenix/config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :hello_phoenix,
+  ecto_repos: [HelloPhoenix.Repo]
+
+# Configures the endpoint
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "91qmZoLgfd1igy1YtQ5elwNXIpRhiTjrSwxnSFdv0zrx4P5jhhZv83vQ6pF/p7I9",
+  render_errors: [view: HelloPhoenix.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: HelloPhoenix.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ hello_phoenix/lib/hello_phoenix/endpoint.ex
@@ -0,0 +1,42 @@
+defmodule HelloPhoenix.Endpoint do
+  use Phoenix.Endpoint, otp_app: :hello_phoenix
+
+  socket "/socket", HelloPhoenix.UserSocket
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/", from: :hello_phoenix, gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+    plug Phoenix.LiveReloader
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Logger
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Poison
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+
+  # The session will be stored in the cookie and signed,
+  # this means its contents can be read but not tampered with.
+  # Set :encryption_salt if you would also like to encrypt it.
+  plug Plug.Session,
+    store: :cookie,
+    key: "_hello_phoenix_key",
+    signing_salt: "MnQ2B4th"
+
+  plug HelloPhoenix.Router
+end
