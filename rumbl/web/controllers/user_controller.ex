c4eafe43bb91f282b0fd098fc4fb92c4a7762c9e
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-03-12 13:01:36 +0600

    Authentication plug and login support
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule Rumbl.UserController do
   alias Rumbl.Repo
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render(conn, "new.html", changeset: changeset)
@@ -13,6 +15,7 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
     {:ok, user} ->
       conn
+      |> Rumbl.Auth.login(user)
       |> put_flash(:info, "#{user.name} created")
       |> redirect(to: user_path(conn, :index))
     {:error, changeset} ->
@@ -21,12 +24,23 @@ defmodule Rumbl.UserController do
   end
 
   def index(conn, _params) do
-    users = Repo.all(User)
-    render(conn, "index.html", users: users)
+      users = Repo.all(User)
+      render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(User, id)
     render(conn, "show.html", user: user)
   end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+   end
 end
