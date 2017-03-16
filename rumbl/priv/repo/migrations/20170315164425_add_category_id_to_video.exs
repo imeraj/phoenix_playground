1eb0bd78ddecc8c24bee2bfbc18f3accf5d1d0f7
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-03-16 12:44:40 +0600

    set on_delete constraint for category_id
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- rumbl/priv/repo/migrations/20170315164425_add_category_id_to_video.exs
+++ rumbl/priv/repo/migrations/20170315164425_add_category_id_to_video.exs
@@ -3,7 +3,7 @@ defmodule Rumbl.Repo.Migrations.AddCategoryIdToVideo do
 
   def change do
       alter table(:videos) do
-          add :category_id, references(:categories)
+          add :category_id, references(:categories, on_delete: :nilify_all)
       end
   end
 end
