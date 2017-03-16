acc0f66019bbf038489f3cbfee5d7285470fc27f
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-03-16 17:03:26 +0600

    started putting code for watching videos
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -20,6 +20,7 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
   end
 
   scope "/manage", Rumbl do
