6b30d6739c8a6fe08eef657d94fb67032d7f2e76
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-03-19 12:31:57 +0600

    Add authentication on channel
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- rumbl/web/plugs/auth.ex
+++ rumbl/web/plugs/auth.ex
@@ -10,13 +10,20 @@ defmodule Rumbl.Auth do
 
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
-    user = user_id && repo.get(Rumbl.User, user_id)
-    assign(conn, :current_user, user)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
+      user = user_id && repo.get(Rumbl.User, user_id) ->
+        put_current_user(conn, user)
+      true ->
+        assign(conn, :current_user, nil)
+    end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -48,5 +55,13 @@ defmodule Rumbl.Auth do
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
+   end
+
+   defp put_current_user(conn, user) do
+     token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+     conn
+     |> assign(:current_user, user)
+     |> assign(:user_token, token)
    end
 end
