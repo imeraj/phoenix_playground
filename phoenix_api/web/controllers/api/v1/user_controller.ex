e5a8ff447211c54b6a01bc283cb3bf0c5435ca30
Mohammad Merajul Islam Molla <merajul.molla@sekai-lab-bd.com>
2017-03-29 17:14:27 +0600

    Fixed json response from registration API
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- phoenix_api/web/controllers/api/v1/user_controller.ex
+++ phoenix_api/web/controllers/api/v1/user_controller.ex
@@ -8,18 +8,15 @@ defmodule PhoenixApi.Api.V1.UserController do
   def create(conn, %{"user" => user_params}) do
     changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
-    {:ok, user} ->
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", user_path(conn, :show, user))
-      |> json("Registration successful")
-    {:error, changeset} ->
-      respose = changeset.errors |> Poison.encode!
-      IO.inspect respose
-      conn
-      |> put_status(:unprocessable_entity)
-      |> json("")
-      |> halt
+      {:ok, user} ->
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", user_path(conn, :show, user))
+        |> render("show.json", user: user)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(PhoenixApi.ChangesetView, "error.json", changeset: changeset)
     end
   end
 end
---
+++ phoenix_api/web/views/api/v1/user_view.ex
@@ -0,0 +1,15 @@
+defmodule PhoenixApi.Api.V1.UserView do
+  use PhoenixApi.Web, :view
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, PhoenixApi.Api.V1.UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, PhoenixApi.Api.V1.UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id}
+  end
+end
