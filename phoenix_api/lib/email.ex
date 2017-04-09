78ef0dfeefc21a3c21f749735a69688dc9c1aa20
Mohammad Merajul Islam Molla <meraj.enigma@gmail.com>
2017-04-09 15:07:26 +0600

    Email sending complete with views and templates
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- phoenix_api/lib/email.ex
+++ phoenix_api/lib/email.ex
@@ -2,19 +2,21 @@ defmodule PhoenixApi.Email do
   import Bamboo.Email
   use Bamboo.Phoenix, view: PhoenixApi.EmailView
 
-  def order_confirmation_text_email(email_address) do
-    new_email
+  def order_confirmation_text_email(email_address, order) do
+    new_email()
     |> to(email_address)
     |> from("no-reply@phoenixapi.org")
     |> subject("Order Confirmation!")
-    |> text_body("Your order is confirmed!")
+    |> put_text_layout({PhoenixApi.LayoutView, "email.text"})
+    |> render("order_confirmation.text", email_address: email_address, order: order)
   end
 
-  def order_confirmation_html_email(email_address) do
-    new_email
+  def order_confirmation_html_email(email_address, order) do
+    new_email()
     |> to(email_address)
     |> from("no-reply@phoenixapi.org")
     |> subject("Order Confirmation!")
-    |> html_body(<strong>"Your order is confirmed!"</strong>)
+    |> put_html_layout({PhoenixApi.LayoutView, "email.html"})
+    |> render("order_confirmation.html", email_address: email_address, order: order)
   end
 end
