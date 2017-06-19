a9d469d642a66c131c0bb258db7543bb94bf4397
iMeraj-MacbookPro <meraj@Enigma-Meraj.local>
2017-06-19 21:59:20 +0600

    negative path testing got video controller added
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- 1.2/rumbl/test/controllers/video_controller_test.exs
+++ 1.2/rumbl/test/controllers/video_controller_test.exs
@@ -6,6 +6,8 @@ defmodule Rumbl.VideoControllerTest do
   @valid_attrs %{description: "some content", title: "some content", url: "some content"}
   @invalid_attrs %{}
 
+	defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
+
 	setup %{conn: conn} = config do
 		if username = config[:login_as] do
 	    user = TestHelpers.insert_user(%{username: username})
@@ -43,5 +45,14 @@ defmodule Rumbl.VideoControllerTest do
 		refute String.contains?(conn.resp_body, other_video.title)
 	end
 
+	@tag login_as: "max"
+	test "does not create video and renders erros when invalid", %{conn: conn} do
+		count_before = video_count(Rumbl.Video)
+		conn = post conn, video_path(conn, :create), video: @invalid_attrs
+
+		assert html_response(conn, 200) =~ "check the errors"
+		count_after = video_count(Rumbl.Video)
+		assert count_after == count_before
+	end
 end
 
