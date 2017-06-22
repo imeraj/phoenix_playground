ca13dde8bc7e6a25ab802e360ed2e42744d6a8fa
iMeraj-MacbookPro <meraj@Enigma-Meraj.local>
2017-06-22 21:45:49 +0600

    minor refactor
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- 1.2/rumbl/web/static/js/player.js
+++ 1.2/rumbl/web/static/js/player.js
@@ -25,7 +25,7 @@ let Player = {
     onPlayerStateChange(event) {},
     getCurrentTime() { return Math.floor(this.player.getCurrentTime() * 1000) },
     sekkTo(millsec) { return this.player.seekTo(millsec/1000) }
+}
 
-    }
+export default Player
 
-    export default Player
--- 1.2/rumbl/web/static/js/video.js
+++ 1.2/rumbl/web/static/js/video.js
@@ -1,64 +1,65 @@
 import Player from "./player"
 
 let Video = {
-    init (socket, element) {
-        if (!element) return
-        let playerId = element.getAttribute("data-player-id")
-        let videoId = element.getAttribute("data-id")
+  init (socket, element) {
+      if (!element) return
+      let playerId = element.getAttribute("data-player-id")
+      let videoId = element.getAttribute("data-id")
 
-        socket.connect()
-        Player.init(element.id, playerId, () => {
-          this.onReady(videoId, socket)
-        })
-      },
+      socket.connect()
+      Player.init(element.id, playerId, () => {
+        this.onReady(videoId, socket)
+      })
+    },
 
-      onReady(videoId, socket) {
-        let msgContainer = document.getElementById("msg-container")
-        let msgInput = document.getElementById("msg-input")
-        let postButton = document.getElementById("msg-submit")
-        let vidChannel = socket.channel("videos:" + videoId)
+  onReady(videoId, socket) {
+    let msgContainer = document.getElementById("msg-container")
+    let msgInput = document.getElementById("msg-input")
+    let postButton = document.getElementById("msg-submit")
+    let vidChannel = socket.channel("videos:" + videoId)
 
-        postButton.addEventListener("click", e => {
-          let payload = {body: msgInput.value, at: Player.getCurrentTime()}
-          vidChannel.push("new_annotation", payload)
-                    .receive("error", e => console.log(e))
-          msgInput.value = ""
-        })
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e))
+      msgInput.value = ""
+    })
 
-        vidChannel.on("new_annotation", (resp) => {
-          vidChannel.params.last_seen_id = resp.id
-          this.renderAnnotation(msgContainer, resp)
-        })
+    vidChannel.on("new_annotation", (resp) => {
+      vidChannel.params.last_seen_id = resp.id
+      this.renderAnnotation(msgContainer, resp)
+    })
 
-        vidChannel.join()
-          .receive("ok", ({annotations}) => {
-            let ids = annotations.map(ann => ann.id)
-            if (ids.length > 0) {
-              vidChannel.params.last_seen_id == Math.max(...ids)
-            }  
-            annotations.forEach(ann => this.renderAnnotation(msgContainer, ann))
-          })
-          .receive("error", reason => console.log("Join failed", reason))
-      },
+    vidChannel.join()
+      .receive("ok", ({annotations}) => {
+        let ids = annotations.map(ann => ann.id)
+        if (ids.length > 0) {
+          vidChannel.params.last_seen_id == Math.max(...ids)
+        }
+        annotations.forEach(ann => this.renderAnnotation(msgContainer, ann))
+      })
+      .receive("error", reason => console.log("Join failed", reason))
+    },
 
-      esc(str) {
-        let div = document.createElement("div")
-        div.appendChild(document.createTextNode(str))
-        return div.innerHTML
-      },
+    esc(str) {
+      let div = document.createElement("div")
+      div.appendChild(document.createTextNode(str))
+      return div.innerHTML
+    },
 
-      renderAnnotation(msgContainer, {user, body, at}) {
-        let template = document.createElement("div")
+    renderAnnotation(msgContainer, {user, body, at}) {
+      let template = document.createElement("div")
 
-        template.innerHTML = `
-          <a href="#" data-seek="${this.esc(at)}">
-          <b>[${at / (60 * 1000)}]:${this.esc(user.username)}</b>:${this.esc(body)}
-          </a>
-          `
+      template.innerHTML = `
+        <a href="#" data-seek="${this.esc(at)}">
+        <b>[${at / (60 * 1000)}]:${this.esc(user.username)}</b>:${this.esc(body)}
+        </a>
+        `
 
-        msgContainer.appendChild(template)
-        msgContainer.scrollTop = msgContainer.scrollHeight
-      }
+      msgContainer.appendChild(template)
+      msgContainer.scrollTop = msgContainer.scrollHeight
     }
+}
 
-    export default Video
+export default Video
+
