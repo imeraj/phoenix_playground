bfc47a7835706dd50efb8b969be1ba34de2c961e
Meraj <meraj.enigma@gmail.com>
2019-01-01 12:24:24 +0900

    Integrated with InfoSys except disabled caching (not working) TODO: Check socket disconnection properly, seems a bug
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- 1.4/rumbl/lib/rumbl/accounts/accounts.ex
+++ 1.4/rumbl/lib/rumbl/accounts/accounts.ex
@@ -8,6 +8,12 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Accounts.User
   alias Rumbl.Repo
 
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert!()
+  end
+
   def list_users do
     Repo.all(User)
   end
--- 1.4/rumbl/lib/rumbl/info_sys/info_sys.ex
+++ 1.4/rumbl/lib/rumbl/info_sys/info_sys.ex
@@ -14,10 +14,10 @@ defmodule Rumbl.InfoSys do
       opts = Keyword.put_new(opts, :limit, 10)
       backends = opts[:backends] || @backends
 
-      {uncached_backends, cached_results} =
-        fetch_cached_results(backends, query, opts)
+      # {uncached_backends, cached_results} =
+      #   fetch_cached_results(backends, query, opts)
 
-      uncached_backends
+      backends
       |> Enum.map(&async_query(&1, query, opts))
       |> Task.yield_many(timeout)
       |> Enum.map(fn {task, res} -> res || Task.shutdown(task, :brutal_kill) end)
@@ -25,10 +25,9 @@ defmodule Rumbl.InfoSys do
         {:ok, results} -> results
         _ -> []
       end)
-      |> write_results_to_cache(query, opts)
-      |> Kernel.++(cached_results)
       |> Enum.sort(&(&1.score >= &2.score))
-      |> Enum.take(opts[:limit])
+      |> Enum.take(opts[:limit]
+      )
     end
 
     defp async_query(backend, query, opts) do
@@ -36,24 +35,24 @@ defmodule Rumbl.InfoSys do
           :compute, [query, opts], shutdown: :brutal_kill)
     end
 
-    defp fetch_cached_results(backends, query, opts) do
-      {uncached_backends, results} =
-        Enum.reduce(
-                   backends,
-                   {[], []},
-                   fn backend, {uncached_backends, acc_results} ->
-                      case Cache.fetch({backend, query, opts[:limit]}) do
-                        {:ok, results} -> {uncached_backends, [results | acc_results]}
-                        :error -> {[backend.name() | uncached_backends], acc_results}
-                      end
-                   end)
-      {uncached_backends, List.flatten(results)}
-    end
+    # defp fetch_cached_results(backends, query, opts) do
+    #   {uncached_backends, results} =
+    #     Enum.reduce(
+    #                backends,
+    #                {[], []},
+    #                fn backend, {uncached_backends, acc_results} ->
+    #                   case Cache.fetch({backend, query, opts[:limit]}) do
+    #                     {:ok, results} -> {uncached_backends, [results | acc_results]}
+    #                     :error -> {[backend.name() | uncached_backends], acc_results}
+    #                   end
+    #                end)
+    #   {uncached_backends, List.flatten(results)}
+    # end
 
-    defp write_results_to_cache(results, query, opts) do
-      Enum.map(results, fn %Result{backend: backend} = result ->
-        :ok = Cache.put({backend.name(), query, opts[:limit]}, result)
-        result
-      end)
-    end
+    # defp write_results_to_cache(results, query, opts) do
+    #   Enum.map(results, fn %Result{backend: backend} = result ->
+    #     :ok = Cache.put({backend.name(), query, opts[:limit]}, result)
+    #     result
+    #   end)
+    # end
 end
