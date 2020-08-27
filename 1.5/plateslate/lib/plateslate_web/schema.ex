bade4b987940a94597e4f6435307b20edb5b2d4b
Meraj <meraj.enigma@gmail.com>
2020-08-27 07:56:09 -0400

    - adding argument in field

--- 1.5/plateslate/lib/plateslate_web/schema.ex
+++ 1.5/plateslate/lib/plateslate_web/schema.ex
@@ -1,11 +1,19 @@
 defmodule PlateslateWeb.Schema do
   use Absinthe.Schema
   alias Plateslate.{Repo, Menu}
+  import Ecto.Query
 
   query do
     field :menu_items, list_of(:menu_item), description: "The list of available menu items" do
-      resolve(fn _, _, _ ->
-        {:ok, Repo.all(Menu.Item)}
+      arg(:matching, :string)
+
+      resolve(fn
+        _, %{matching: name}, _ when is_binary(name) ->
+          query = from t in Menu.Item, where: ilike(t.name, ^"%#{name}%")
+          {:ok, Repo.all(query)}
+
+        _, _, _ ->
+          {:ok, Repo.all(Menu.Item)}
       end)
     end
   end
