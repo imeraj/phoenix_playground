011f7f14c3568e5d8e9ef7e5404585382e935735
Meraj <meraj.enigma@gmail.com>
2021-10-24 19:35:38 -0400

    - new hello app

---
+++ 1.6.2/hello/config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :hello, Hello.Repo,
+  username: "meraj",
+  password: "",
+  database: "hello_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello, HelloWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "DQDHaHBLY6UejCkRSAfw6N6LZb0xouTlRGdTyrXM8urenlf0C4mIqSdk4UUkb2ye",
+  server: false
+
+# In test we don't send emails.
+config :hello, Hello.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
