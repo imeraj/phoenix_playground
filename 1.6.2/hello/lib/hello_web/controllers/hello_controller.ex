fad1450405f7771f15144f1f864ca47f5311cc1f
Meraj <meraj.enigma@gmail.com>
2021-10-25 18:46:02 -0400

    - add new pages

---
+++ 1.6.2/hello/lib/hello_web/controllers/hello_controller.ex
@@ -0,0 +1,11 @@
+defmodule HelloWeb.HelloController do
+  use HelloWeb, :controller
+
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
+  end
+end
--- 1.6.2/hello/lib/hello_web/router.ex
+++ 1.6.2/hello/lib/hello_web/router.ex
@@ -18,6 +18,8 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/hello", HelloController, :index
+    get "/hello/:messenger", HelloController, :show
   end
 
   # Other scopes may use custom stacks.
